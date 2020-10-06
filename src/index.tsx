ed1a046bcd05f7cd641539f60231d282b669ce4b
Sergio Correia <sergiosdcorreia@gmail.com>
2020-10-06 21:45:28 +0100

    Creating styles themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { unregister } from './core/index'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <div>Hello world!</div>
-  </>,
+  </ThemeProvider>,
   document.getElementById('root')
 )
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,24 +1,26 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 export default createGlobalStyle`
-    html {
+${(props) => css`
+  html {
+    height: 100%;
+
+    body {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      margin: 0;
+
+      #root {
+        background: radial-gradient(#282c34cc, #282c34);
+        color: #282c34;
+        display: flex;
+        font-family: sans-serif;
         height: 100%;
-
-        body {
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-            margin: 0;
-
-            #root {
-                background: radial-gradient(#282c34cc, #282c34);
-                color: #282c34;
-                display: flex;
-                font-family: sans-serif;
-                height: 100%;
-                justify-content: center;
-                padding: 15px;
-            }
-        }
+        justify-content: center;
+        padding: 15px;
+      }
     }
+  }
+`}
 `
